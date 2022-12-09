2e5e03c560091e1eb760f977df89c7cf68a2435d
Anthony Dodd <dodd.anthonyjosiah@gmail.com>
2022-12-09 13:32:00 -0600

    Add gRPC layer
    Almost the same in every way, except that it uses Tower's
    `TraceLayer::new_for_grpc`, and formats the initially generated span a
    bit differently to match gRPC conventions.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@
 mod middleware;
 mod tools;
 
-pub use self::middleware::opentelemetry_tracing_layer;
+pub use self::middleware::{opentelemetry_tracing_layer, opentelemetry_tracing_layer_grpc};
 pub use self::tools::*;
